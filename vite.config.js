68725a75997158d779638915d9753e974336b58c
Gopi Raj <gopiraj@elitoinnovations.com>
2026-02-19 09:53:00 +0530

    updated name

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   // base: "/", //   for netlify server 
-  base: "/javascript-compiler",
+  base: "/js-c",
   plugins: [react()],
 })
